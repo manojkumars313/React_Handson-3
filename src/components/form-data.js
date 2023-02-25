ea325ea93d19bb23e9a0afdecfe14238b4bb5566
manojkumars313 <manojraj.mm313@gmail.com>
2023-02-25 15:15:28 +0530

    react Handson-3

---
+++ src/components/form-data.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class FormData extends Component {
+  render() {
+    return <div id="display-form-data">{data}</div>;
+  }
+}
+
+export default FormData;
